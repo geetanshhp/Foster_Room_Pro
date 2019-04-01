ed1c3229e9ed22f796a1a0653df352f5e3539cc0
Geetansh Sharma <geetanshsharmahp@gmail.com>
2019-04-01 20:48:17 +0530

    April_Fool

--- app/src/main/java/com/example/geetanshsharma/foster_room/MainActivity.java
+++ app/src/main/java/com/example/geetanshsharma/foster_room/MainActivity.java
@@ -2,22 +2,52 @@ package com.example.geetanshsharma.foster_room;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
 {
-
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    NavigationView navigationView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.navigationview);
+        drawerLayout =findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.open_drawer,R.string.close_drawer);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        navigationView=findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(this);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         BottomNavigationView bottomNavigationView=findViewById(R.id.navigation_view);
         bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
     private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener=new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -44,4 +74,30 @@ public class MainActivity extends AppCompatActivity
             return true;
         }
     };
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item)
+    {
+        Fragment selected =null;
+
+        switch (item.getItemId())
+        {
+            case R.id.aabout_us:
+                selected =new About_us();
+                break;
+            case R.id.id_get_help:
+                selected =new Help_Frag();
+                break;
+            case R.id.id_notification:
+                selected =new Notification_fragment();
+                break;
+            case R.id.feedback:
+                selected =new Help_Frag();
+                break;
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.frames,selected).commit();
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+
+    }
 }
--- app/src/main/java/com/example/geetanshsharma/foster_room/Splash_Screen.java
+++ app/src/main/java/com/example/geetanshsharma/foster_room/Splash_Screen.java
@@ -3,6 +3,8 @@ package com.example.geetanshsharma.foster_room;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Splash_Screen extends AppCompatActivity {
 
@@ -10,6 +12,8 @@ public class Splash_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash__screen);
 
         //splash_Screeen
